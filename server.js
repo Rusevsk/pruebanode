05c14a77d774b949f822ab2ac9f997500e3a89e4
Rusevsk <99459871+Rusevsk@users.noreply.github.com>
2025-03-23 11:30:09 -0500

    Cambio automatico

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('¡Hola desde el proyecto de prueba sin bcrypt!');
+    res.send('¡Hola, este es el cambio automático para la tarea!');
 });
 
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
